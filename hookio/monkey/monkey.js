f4924851461df41d99be81eef0a75d744193563c
J.Fernando Sánchez <balkian@gmail.com>
2012-03-20 13:07:16 +0100

    Added simple monkey

---
+++ hookio/monkey/monkey.js
@@ -0,0 +1,74 @@
+var Hook = require('hook.io').Hook,
+    express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib'),
+    app = express.createServer(),
+    io = require('socket.io').listen(app);
+
+    app.configure(function () {
+      app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+      app.use(express.static(__dirname + '/public'));
+      app.set('views', __dirname);
+      app.set('view engine', 'jade');
+
+      function compile (str, path) {
+        return stylus(str)
+          .set('filename', path)
+          .use(nib());
+      };
+    })
+
+    app.get('/', function (req, res) {
+      res.render('index', { layout: false });
+    });
+
+/*
+ * Hook
+ */
+var monkey = new Hook({ 
+  name: "monkey-hook",
+  debug: true
+});
+
+monkey.on('hook::ready', function () {
+  console.log('I am online :D');
+});
+    /**
+     * App listen.
+     */
+
+app.listen(3000, function () {
+    var addr = app.address();
+    console.log('app listening on http://' + addr.address + ':' + addr.port);
+});
+
+monkey.connect(1337);
+
+var nicknames = {};
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('user message', function (data) {
+        console.log(data);
+    });
+    socket.on('command', function (data,fn) {
+        var name = data['name'];
+        var payload = data['payload'];
+        console.log('Executing '+name+' with '+payload);
+        monkey.emit(name,payload,fn);
+    });
+    socket.on('nickname', function (nick, fn) {
+        if (nicknames[nick]) {
+          fn(true);
+        } else {
+            fn(false);
+            socket.nickname = nick;
+            nicknames[nick] = socket;
+        }
+    });
+    socket.on('disconnect', function () {
+        if (!socket.nickname) return;
+        delete nicknames[socket.nickname];
+        });
+});
+
